0ab585a30895958b34c5d2e46c720d0f4754b6f1
Dargor94 <franciscomasera1994@gmail.com>
2023-08-22 15:14:45 -0300

    JaegerConfig

---
+++ src/main/java/org/dargor/product/app/config/JaegerConfig.java
@@ -0,0 +1,26 @@
+package org.dargor.product.app.config;
+
+import io.jaegertracing.internal.JaegerTracer;
+import io.jaegertracing.internal.samplers.ConstSampler;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JaegerConfig {
+
+    @Value("${spring.application.name}")
+    private  String appName;
+
+    @Bean
+    public JaegerTracer jaegerTracer() {
+
+        return new io.jaegertracing.Configuration(appName)
+                .withSampler(new io.jaegertracing.Configuration.SamplerConfiguration().withType(ConstSampler.TYPE)
+                        .withParam(1))
+                .withReporter(new io.jaegertracing.Configuration.ReporterConfiguration().withLogSpans(true))
+                .getTracer();
+    }
+
+
+}
