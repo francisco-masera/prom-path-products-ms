8221b3986f0ec42d34b7fac3f99e2d288d72f78b
Dargor94 <franciscomasera1994@gmail.com>
2023-10-19 21:32:58 -0300

    redis serialization fix

--- src/main/java/org/dargor/product/app/util/RedisUtil.java
+++ src/main/java/org/dargor/product/app/util/RedisUtil.java
@@ -1,6 +1,7 @@
 package org.dargor.product.app.util;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import lombok.AllArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class RedisUtil<T> {
 
-    private static final Gson GSON = new Gson();
+    private static final Gson GSON = new GsonBuilder().disableInnerClassSerialization().create();
     private final RedisTemplate<String, String> redisTemplate;
 
     public void storeValues(final String key, T value, int expire) {
